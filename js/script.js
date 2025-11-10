35d8f695f20fd19d960375c22336b217585a7c6b
torgomyan01 <and.torgomyan01@gmail.com>
2025-11-10 17:53:15 +0400

    Add slider

--- js/script.js
+++ js/script.js
@@ -11,10 +11,38 @@ $(window).on("resize", function () {
   blockNewsContentDescription.height(blockNewsContentMain.height());
 });
 
-$(".slider-laboratory").slick({
+$("#slider-laboratory").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: ".slider-laboratory-nav",
+  dots: true,
+});
+
+$("#slider-popular-products").slick({
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  prevArrow: $(".block-popular-products-top-block-slide-icon.prev"),
+  nextArrow: $(".block-popular-products-top-block-slide-icon.next"),
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
 });
